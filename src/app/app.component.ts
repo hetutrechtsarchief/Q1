f6acc6a1afff912de2391ae48695ff4502b8ee91
Ivar Troost <ivar@desagaz.com>
2019-11-29 23:19:23 +0100

    Install leaflet and implement local demo of image map

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import * as L from 'leaflet';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Q1';
+export class AppComponent implements OnInit {
+  ngOnInit() {
+    const map = L.map('map', { crs: L.CRS.Simple }).setView([3, 3], 0);
+
+    // @ts-ignore
+    L.tileLayer('/assets/demo/z{z}_x{x}_y{y}.jpg', {
+      maxNativeZoom: 2,
+      maxZoom: 10
+    }).addTo(map);
+  }
 }
