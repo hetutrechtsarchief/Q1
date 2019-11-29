422bbd5f08d0e833363e8089ba8869d96352d57d
Simon Dirks <s.b.dirks@students.uu.nl>
2019-11-29 23:47:06 +0100

    First version of entry modal

---
+++ src/app/components/entry-modal/entry-modal.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-entry-modal',
+  templateUrl: './entry-modal.component.html',
+  styleUrls: ['./entry-modal.component.scss']
+})
+
+
+
+export class EntryModalComponent implements OnInit {
+  constructor() { }
+
+  ngOnInit() {
+    // alert('djkfaslj');
+  }
+
+}
