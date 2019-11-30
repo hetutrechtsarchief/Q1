d6c72e1b6789b325ffb776a630a94c13ea85e4ce
Simon Dirks <s.b.dirks@students.uu.nl>
2019-11-30 06:45:22 +0100

    Retrieve description via SPARQL query

--- src/app/components/entry-modal/entry-modal.component.ts
+++ src/app/components/entry-modal/entry-modal.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {SparqlService} from '../../services/sparql.service';
+import {environment} from '../../../environments/environment';
 
 @Component({
   selector: 'app-entry-modal',
@@ -10,13 +12,27 @@ import { Component, OnInit } from '@angular/core';
 
 export class EntryModalComponent implements OnInit {
   addedLinks = [];
-  constructor() { }
+  beeldbankGuid = '8744DB4D363D5B43BE56BFD03A251768';
+  imageUrl = 'https://proxy.archieven.nl/download/39/' + this.beeldbankGuid;
 
+  constructor(private sparql: SparqlService) { }
+
+  async retrieveDescription() {
+    let query: string;
+    query = `
+      SELECT ?description WHERE {
+        <${'https://hetutrechtsarchief.nl/id/' + this.beeldbankGuid}> dc:description ?description .
+      } LIMIT 100
+    `;
+    const queryResult = await this.sparql.query(environment.sparqlEndpoints.HuaBeeldbank, `${environment.sparqlPrefixes.HuaBeeldbank} ${query}`);
+    $('#description').html('<i>Beschrijving</i>: ' + queryResult.results.bindings[0].description.value);
+  }
   ngOnInit() {
+  this.retrieveDescription();
 
-    ($( '#add-object-input' ) as any).autocomplete({
+  ($( '#add-object-input' ) as any).autocomplete({
       source( request, response ) {
-        $('#add-object-selected-id').text("Searching for matching terms...");
+        $('#add-object-selected-id').text('Searching for matching terms...');
 
         $.ajax( {
           url: 'http://demo.netwerkdigitaalerfgoed.nl:8080/nde/graphql',
@@ -52,7 +68,7 @@ export class EntryModalComponent implements OnInit {
     const object = $('#add-object-input').val();
     const objectURL = $('#add-object-selected-id a').attr('href');
     const predicate = $('#predicate-select').val();
-    this.addedLinks.push({'predicate': predicate, 'object': object, 'url': objectURL});
+    this.addedLinks.push({predicate: predicate, object: object, url: objectURL});
   }
 
 }
