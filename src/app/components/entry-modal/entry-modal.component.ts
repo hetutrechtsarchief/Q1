c12ce2dba694a7f1c313a2e39914779aaaf4254b
Simon Dirks <s.b.dirks@students.uu.nl>
2019-11-30 05:16:57 +0100

    Added list of current links

--- src/app/components/entry-modal/entry-modal.component.ts
+++ src/app/components/entry-modal/entry-modal.component.ts
@@ -9,12 +9,14 @@ import { Component, OnInit } from '@angular/core';
 
 
 export class EntryModalComponent implements OnInit {
+  addedLinks = [];
   constructor() { }
 
   ngOnInit() {
-
     $( '#add-object-input' ).autocomplete({
       source( request, response ) {
+        $('#add-object-selected-id').text("Searching for matching terms...");
+        
         $.ajax( {
           url: 'http://demo.netwerkdigitaalerfgoed.nl:8080/nde/graphql',
           contentType: 'application/json',
@@ -29,7 +31,7 @@ export class EntryModalComponent implements OnInit {
             console.log(resultData);
             for (let i = 0; i < resultData.terms.length - 1; i++) {
               prefLabels[i] = {
-                label: resultData.terms[i].prefLabel.join() + " (" + resultData.terms[i].altLabel.join() + ")",
+                label: resultData.terms[i].prefLabel.join() + ' (' + resultData.terms[i].altLabel.join() + ')',
                 wikiDataUri: resultData.terms[i].uri
               };
             }
@@ -39,32 +41,17 @@ export class EntryModalComponent implements OnInit {
       },
       minLength: 3,
       select( event, ui ) {
-        $("#add-object-selected-id").html('Selected: <a target="_blank" href="' + ui.item.wikiDataUri + '">' + ui.item.wikiDataUri + '</a>');
+        $('#add-object-selected-id').html('Selected: <a target="_blank" href="' + ui.item.wikiDataUri + '">' + ui.item.wikiDataUri + '</a>');
         // console.log( 'Selected: ' + ui.item.value + ' aka ' + ui.item.wikiDataUri );
       }
     });
+  }
 
-    // alert('djkfaslj');
-    // $('#add-object-input').on('input', function() {
-    //   const keyword = $(this).val();
-    //
-    //   $.ajax({url: "http://demo.netwerkdigitaalerfgoed.nl:8080/nde/graphql",
-    //     contentType: "application/json",type:'POST',
-    //     data: JSON.stringify({ query: 'query {\n' +
-    //         '  terms(match:"*' + keyword + '*" dataset:["wikidata"]) { dataset terms {uri prefLabel altLabel} }\n' +
-    //         '}',
-    //     variables: null}),
-    //     success: function(result) {
-    //       console.log(JSON.stringify(result))
-    //     }
-    //   });
-    // });
-    // add-object-input
-    // http://demo.netwerkdigitaalerfgoed.nl:8080/nde/graphql
-
-    // query {
-    //   terms(match:"*fietsen*" dataset:["wikidata"]) { dataset terms {uri prefLabel altLabel} }
-    // }
+  onAddLink() {
+    const object = $('#add-object-input').val();
+    const objectURL = $('#add-object-selected-id a').attr('href');
+    const predicate = $('#predicate-select').val();
+    this.addedLinks.push({'predicate': predicate, 'object': object, 'url': objectURL});
   }
 
 }
